fe0dcceb0161f752d49a6408afa0bdf1a7df710e
nicolas-rigal <115165951+nicolas-rigal@users.noreply.github.com>
2023-02-24 07:46:10 +0100

    Update pygame_sonic_snake.py

--- pygame_sonic_snake.py
+++ pygame_sonic_snake.py
@@ -65,8 +65,7 @@ snake_position = [100, 50]
 # defining first 4 blocks of snake body
 snake_body = [[100, 50],
 			[90, 50],
-			[80, 50],
-			[70, 50]
+			[80, 50]
 			]
 # fruit position
 fruit_position = [random.randrange(1, (window_x//10)) * 10,
@@ -139,80 +138,80 @@ runnig = True
 # Main Function
 while runnig:
 
-	draw_damier()
-	
-        # handling key events
-        for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_UP:
-                    change_to = 'UP'
-                if event.key == pygame.K_DOWN:
-                    change_to = 'DOWN'
-                if event.key == pygame.K_LEFT:
-                    change_to = 'LEFT'
-                if event.key == pygame.K_RIGHT:
-                    change_to = 'RIGHT'
+    draw_damier()
 
-        # If two keys pressed simultaneously
-        # we don't want snake to move into two
-        # directions simultaneously
-        if change_to == 'UP' and direction != 'DOWN':
-            direction = 'UP'
-        if change_to == 'DOWN' and direction != 'UP':
-            direction = 'DOWN'
-        if change_to == 'LEFT' and direction != 'RIGHT':
-            direction = 'LEFT'
-        if change_to == 'RIGHT' and direction != 'LEFT':
-            direction = 'RIGHT'
+    # handling key events
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                change_to = 'UP'
+            if event.key == pygame.K_DOWN:
+                change_to = 'DOWN'
+            if event.key == pygame.K_LEFT:
+                change_to = 'LEFT'
+            if event.key == pygame.K_RIGHT:
+                change_to = 'RIGHT'
 
-        # Moving the snake
-        if direction == 'UP':
-            snake_position[1] -= 10
-        if direction == 'DOWN':
-            snake_position[1] += 10
-        if direction == 'LEFT':
-            snake_position[0] -= 10
-        if direction == 'RIGHT':
-            snake_position[0] += 10
+    # If two keys pressed simultaneously
+    # we don't want snake to move into two
+    # directions simultaneously
+    if change_to == 'UP' and direction != 'DOWN':
+        direction = 'UP'
+    if change_to == 'DOWN' and direction != 'UP':
+        direction = 'DOWN'
+    if change_to == 'LEFT' and direction != 'RIGHT':
+        direction = 'LEFT'
+    if change_to == 'RIGHT' and direction != 'LEFT':
+        direction = 'RIGHT'
 
-        # Snake body growing mechanism
-        # if fruits and snakes collide then scores
-        # will be incremented by 10
-        snake_body.insert(0, list(snake_position))
-        if snake_position[0] == fruit_position[0] and snake_position[1] == fruit_position[1]:
-            score += 10
-            fruit_spawn = False
-        else:
-            snake_body.pop()
-            
-        if not fruit_spawn:
-            fruit_position = [random.randrange(1, (window_x//10)) * 10,
-                            random.randrange(1, (window_y//10)) * 10]
-            
-        fruit_spawn = True
-    
-        for pos in snake_body:
-            pygame.draw.rect(game_window, sonic,
-                            pygame.Rect(pos[0], pos[1], 10, 10))
-        pygame.draw.rect(game_window, goldenyellow, pygame.Rect(
-            fruit_position[0], fruit_position[1], 10, 10))
+    # Moving the snake
+    if direction == 'UP':
+        snake_position[1] -= 10
+    if direction == 'DOWN':
+        snake_position[1] += 10
+    if direction == 'LEFT':
+        snake_position[0] -= 10
+    if direction == 'RIGHT':
+        snake_position[0] += 10
 
-        # Game Over conditions
-        if snake_position[0] < 0 or snake_position[0] > window_x-10:
+    # Snake body growing mechanism
+    # if fruits and snakes collide then scores
+    # will be incremented by 10
+    snake_body.insert(0, list(snake_position))
+    if snake_position[0] == fruit_position[0] and snake_position[1] == fruit_position[1]:
+        score += 10
+        fruit_spawn = False
+    else:
+        snake_body.pop()
+        
+    if not fruit_spawn:
+        fruit_position = [random.randrange(1, (window_x//10)) * 10,
+                        random.randrange(1, (window_y//10)) * 10]
+        
+    fruit_spawn = True
+
+    for pos in snake_body:
+        pygame.draw.rect(game_window, sonic,
+                        pygame.Rect(pos[0], pos[1], 10, 10))
+    pygame.draw.rect(game_window, goldenyellow, pygame.Rect(
+        fruit_position[0], fruit_position[1], 10, 10))
+
+    # Game Over conditions
+    if snake_position[0] < 0 or snake_position[0] > window_x-10:
+        game_over()
+    if snake_position[1] < 0 or snake_position[1] > window_y-10:
+        game_over()
+
+    # Touching the snake body
+    for block in snake_body[1:]:
+        if snake_position[0] == block[0] and snake_position[1] == block[1]:
             game_over()
-        if snake_position[1] < 0 or snake_position[1] > window_y-10:
-            game_over()
 
-        # Touching the snake body
-        for block in snake_body[1:]:
-            if snake_position[0] == block[0] and snake_position[1] == block[1]:
-                game_over()
+    # displaying score countinuously
+    show_score(1, white, 'arial', 20)
 
-        # displaying score countinuously
-        show_score(1, white, 'arial', 20)
+    # Refresh game screen
+    pygame.display.update()
 
-        # Refresh game screen
-        pygame.display.update()
-
-        # Frame Per Second /Refresh Rate
-        fps.tick(snake_speed)
+    # Frame Per Second /Refresh Rate
+    fps.tick(snake_speed)
